874c18917567985e9c9d4caf03a35daa72773785
freakstein1 <sinic.dejan@gmail.com>
2019-01-11 21:19:36 +0100

    Add files via upload

---
+++ Assets/Scripts/canvasScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class canvasScript : MonoBehaviour {
+	
+	GameObject flat_terrain_button;
+	GameObject rough_terrain_button;
+	
+	// Use this for initialization
+	void Start () {
+		flat_terrain_button = GameObject.Find("flat_terrain");
+		rough_terrain_button = GameObject.Find("rough_terrain");
+		flat_terrain_button.SetActive(false); 
+		rough_terrain_button.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
